63600047a9b135a6e7c19403503c3ad120e51693
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-15 08:41:51 +0000

    Upload New File

---
+++ src/main/java/com/sapient/coc/application/pricingservice/feign/client/FulfillmentServiceClient.java
@@ -0,0 +1,24 @@
+package com.sapient.coc.application.pricingservice.feign.client;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+
+import com.sapient.coc.application.pricingservice.bo.vo.Fulfillment;
+import com.sapient.coc.application.pricingservice.feign.FeignConfigurationPricing;
+import com.sapient.coc.application.pricingservice.feign.fallback.FulfillmentServiceFalBack;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+@FeignClient(name = "fulfillment-service", url = "http://23.251.147.154:12787", fallback = FulfillmentServiceFalBack.class, configuration = FeignConfigurationPricing.class)
+@RibbonClient(name = "fulfillment-service")
+public interface FulfillmentServiceClient {
+
+	@RequestLine("GET v1/fulfillment/methods")
+	@Headers({ "Authorization:{token}", "Accept: application/json" })
+	ResponseEntity<Fulfillment> getOrderFulFillmentDeatils(@Param("token") String token);
+
+}
+
