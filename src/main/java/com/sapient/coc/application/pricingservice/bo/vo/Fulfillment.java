de124c4042830d965a747c53d66149ebeb5f5bb2
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-15 08:40:13 +0000

    Upload New File

---
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Fulfillment.java
@@ -0,0 +1,41 @@
+package com.sapient.coc.application.pricingservice.bo.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Fulfillment {
+
+	private int code;
+	private String status;
+	private Data data;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+	
+	@JsonProperty("traceId")
+	private String traceid;
+
+	@JsonProperty("spanId")
+	private String spanid;
+
+}
