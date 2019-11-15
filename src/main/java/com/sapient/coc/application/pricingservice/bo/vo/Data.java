232ebf211d3c9e578f582a6e6a5aa94c335cb3e7
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-15 08:39:49 +0000

    Upload New File

---
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Data.java
@@ -0,0 +1,75 @@
+package com.sapient.coc.application.pricingservice.bo.vo;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty ;
+
+
+public class Data {
+
+	private String id;
+    @JsonProperty("lastModified")
+	private String lastModified;
+    @JsonProperty("createdAt")
+	private Date createdAt;
+    @JsonProperty("orderStatus")
+	private String orderStatus;
+    @JsonProperty("userId")
+	private String userId;
+    @JsonProperty("orderItems")
+	private List<FulfillmentItem> fulfillmentItems;
+    public void setId(String id) {
+         this.id = id;
+     }
+     public String getId() {
+         return id;
+     }
+
+	public String getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(String lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public List<FulfillmentItem> getFulfillmentItems() {
+		return fulfillmentItems;
+	}
+
+	public void setFulfillmentItems(List<FulfillmentItem> fulfillmentItems) {
+		this.fulfillmentItems = fulfillmentItems;
+	}
+
+
+
+
+
+
+
+}
