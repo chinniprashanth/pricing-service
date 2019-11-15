db2c942b0c45b4353a5bc8aa85b33b35ac7dea86
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-15 08:42:22 +0000

    Upload New File

---
+++ src/main/java/com/sapient/coc/application/pricingservice/feign/fallback/FulfillmentServiceFalBack.java
@@ -0,0 +1,18 @@
+package com.sapient.coc.application.pricingservice.feign.fallback;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import com.sapient.coc.application.pricingservice.bo.vo.Fulfillment;
+import com.sapient.coc.application.pricingservice.feign.client.FulfillmentServiceClient;
+
+@Component
+public class FulfillmentServiceFalBack implements FulfillmentServiceClient {
+
+	@Override
+	public ResponseEntity<Fulfillment> getOrderFulFillmentDeatils(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
