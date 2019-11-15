38a943e00aec6e322c8220ae10e64f188dc1ab55
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-15 08:38:28 +0000

    Update CartResponse.java

--- src/main/java/com/sapient/coc/application/pricingservice/bo/vo/CartResponse.java
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/CartResponse.java
@@ -106,4 +106,66 @@ public class CartResponse implements Serializable {
 		value.append(total + "-");
 		return value.toString();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(actualTotal);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + ((orderPromotionDescription == null) ? 0 : orderPromotionDescription.hashCode());
+		result = prime * result + ((segmentPromotionDescription == null) ? 0 : segmentPromotionDescription.hashCode());
+		temp = Double.doubleToLongBits(shipping);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(subtotal);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(tax);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(total);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(totalDiscount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CartResponse other = (CartResponse) obj;
+		if (Double.doubleToLongBits(actualTotal) != Double.doubleToLongBits(other.actualTotal))
+			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		if (orderPromotionDescription == null) {
+			if (other.orderPromotionDescription != null)
+				return false;
+		} else if (!orderPromotionDescription.equals(other.orderPromotionDescription))
+			return false;
+		if (segmentPromotionDescription == null) {
+			if (other.segmentPromotionDescription != null)
+				return false;
+		} else if (!segmentPromotionDescription.equals(other.segmentPromotionDescription))
+			return false;
+		if (Double.doubleToLongBits(shipping) != Double.doubleToLongBits(other.shipping))
+			return false;
+		if (Double.doubleToLongBits(subtotal) != Double.doubleToLongBits(other.subtotal))
+			return false;
+		if (Double.doubleToLongBits(tax) != Double.doubleToLongBits(other.tax))
+			return false;
+		if (Double.doubleToLongBits(total) != Double.doubleToLongBits(other.total))
+			return false;
+		if (Double.doubleToLongBits(totalDiscount) != Double.doubleToLongBits(other.totalDiscount))
+			return false;
+		return true;
+	}
 }
