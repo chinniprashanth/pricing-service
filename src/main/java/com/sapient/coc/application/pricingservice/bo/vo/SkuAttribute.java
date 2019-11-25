70343f7af21f1f9388a49f2b7205eb63b8b5cca6
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-25 12:10:15 +0000

    Update SkuAttribute.java

--- src/main/java/com/sapient/coc/application/pricingservice/bo/vo/SkuAttribute.java
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/SkuAttribute.java
@@ -5,31 +5,30 @@ package com.sapient.coc.application.pricingservice.bo.vo;
  *
  */
 public class SkuAttribute {
-    private String name;
-    private String value;
+	private String name;
+	private String value;
 
+	public SkuAttribute() {
+	}
 
-    public SkuAttribute() {
-    }
+	public SkuAttribute(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
 
-    public SkuAttribute(String name, String value) {
-        this.name = name;
-        this.value = value;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
