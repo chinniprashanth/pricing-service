64e8538c80f77d19fb5b821b8345176bef4fdc2b
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-25 12:05:46 +0000

    Update Inventory.java

--- src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Inventory.java
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Inventory.java
@@ -6,30 +6,30 @@ package com.sapient.coc.application.pricingservice.bo.vo;
  */
 public class Inventory {
 
-    private String availabilityStatus;
-    private String url;
+	private String availabilityStatus;
+	private String url;
 
-    public Inventory() {
-    }
+	public Inventory() {
+	}
 
-    public Inventory(String availabilityStatus, String url) {
-        this.availabilityStatus = availabilityStatus;
-        this.url = url;
-    }
+	public Inventory(String availabilityStatus, String url) {
+		this.availabilityStatus = availabilityStatus;
+		this.url = url;
+	}
 
-    public String getAvailabilityStatus() {
-        return availabilityStatus;
-    }
+	public String getAvailabilityStatus() {
+		return availabilityStatus;
+	}
 
-    public void setAvailabilityStatus(String availabilityStatus) {
-        this.availabilityStatus = availabilityStatus;
-    }
+	public void setAvailabilityStatus(String availabilityStatus) {
+		this.availabilityStatus = availabilityStatus;
+	}
 
-    public String getUrl() {
-        return url;
-    }
+	public String getUrl() {
+		return url;
+	}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
