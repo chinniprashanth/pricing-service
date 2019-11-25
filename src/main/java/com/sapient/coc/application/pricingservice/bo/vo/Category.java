8d1e90f19c87a0c1de67714d501dc951a2325f3f
Pooja Yadav <pooyadav@publicisgroupe.net>
2019-11-25 12:05:13 +0000

    Update Category.java

--- src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Category.java
+++ src/main/java/com/sapient/coc/application/pricingservice/bo/vo/Category.java
@@ -5,68 +5,64 @@ package com.sapient.coc.application.pricingservice.bo.vo;
  */
 public class Category {
 
-  private String id;
-  private String name;
-  private String href;
-  private Category parent;
+	private String id;
+	private String name;
+	private String href;
+	private Category parent;
 
-  public Category() {
-  }
+	public Category() {
+	}
 
-  public Category(String id){
-    this.id = id;
-  }
+	public Category(String id) {
+		this.id = id;
+	}
 
-  public Category(String id, String name) {
-    this.id = id;
-    this.name = name;
-  }
+	public Category(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-  public Category(String id, String name, Category parent) {
-    this.id = id;
-    this.name = name;
-    this.parent = parent;
-  }
+	public Category(String id, String name, Category parent) {
+		this.id = id;
+		this.name = name;
+		this.parent = parent;
+	}
 
-  public String getId() {
-    return id;
-  }
+	public String getId() {
+		return id;
+	}
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getHref() {
-    return href;
-  }
+	public String getHref() {
+		return href;
+	}
 
-  public void setHref(String href) {
-    this.href = href;
-  }
+	public void setHref(String href) {
+		this.href = href;
+	}
 
-  public Category getParent() {
-    return parent;
-  }
+	public Category getParent() {
+		return parent;
+	}
 
-  public void setParent(Category parent) {
-    this.parent = parent;
-  }
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
 
-  @Override
-  public String toString() {
-    return "Category{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            ", href='" + href + '\'' +
-            ", parent=" + parent +
-            '}';
-  }
+	@Override
+	public String toString() {
+		return "Category{" + "id='" + id + '\'' + ", name='" + name + '\'' + ", href='" + href + '\'' + ", parent="
+				+ parent + '}';
+	}
 }
