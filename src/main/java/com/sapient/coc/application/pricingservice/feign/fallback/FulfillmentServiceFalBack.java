4956ad6a1922ab9b84e8bcc4b6e05d0166ed46e5
Pooja <pooyadav@publicissapient.com>
2019-11-30 15:49:45 +0530

    adding aaddress vo

--- src/main/java/com/sapient/coc/application/pricingservice/feign/fallback/FulfillmentServiceFalBack.java
+++ src/main/java/com/sapient/coc/application/pricingservice/feign/fallback/FulfillmentServiceFalBack.java
@@ -3,6 +3,7 @@ package com.sapient.coc.application.pricingservice.feign.fallback;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import com.sapient.coc.application.pricingservice.bo.vo.AddressFulfillment;
 import com.sapient.coc.application.pricingservice.bo.vo.Fulfillment;
 import com.sapient.coc.application.pricingservice.feign.client.FulfillmentServiceClient;
 
@@ -10,7 +11,7 @@ import com.sapient.coc.application.pricingservice.feign.client.FulfillmentServic
 public class FulfillmentServiceFalBack implements FulfillmentServiceClient {
 
 	@Override
-	public ResponseEntity<Fulfillment> getOrderFulFillmentDeatils(String token) {
+	public ResponseEntity<Fulfillment> getOrderFulFillmentDeatils(String token, AddressFulfillment address) {
 		// TODO Auto-generated method stub
 		return null;
 	}
